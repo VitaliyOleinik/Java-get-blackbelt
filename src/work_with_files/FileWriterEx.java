b6faa3a4fb7b4fb1e773e833247d05ff3ac38be8
VitaliyOleinik <oleynik624@gmail.com>
2023-07-12 12:04:54 +0600

    FileWriter Example

---
+++ src/work_with_files/FileWriterEx.java
@@ -0,0 +1,34 @@
+package work_with_files;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriterEx {
+    public static void main(String[] args) throws IOException {
+
+        String s = "Наш мир постоянно меняется и развивается. " +
+                "Технологии становятся все более инновационными, " +
+                "а наука открывает новые горизонты. Люди стремятся к" +
+                " лучшей жизни и ищут пути для достижения успеха. " +
+                "Однако, весь этот прогресс не должен заслонять нашу " +
+                "способность быть добрыми и заботливыми друг к другу." +
+                " Важно помнить о значимости межличностных отношений и " +
+                "поддержке близких людей. Только объединившись и " +
+                "поддерживая друг друга, мы сможем создать лучшее будущее " +
+                "для всех.";
+
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter("/home/kardin/development/Java-get-blackbelt/files/test1.txt", true);
+            for (int i = 0; i < s.length(); i++) {
+                writer.write(s.charAt(i));
+            }
+            writer.write(s);
+            System.out.println("Done!");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            writer.close();
+        }
+    }
+}
