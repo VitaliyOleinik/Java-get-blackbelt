17a2e9a8e100d4523fd638946caa4dba2afd0b9c
VitaliyOleinik <oleynik624@gmail.com>
2023-07-13 12:09:44 +0600

    Serialization, transient, serialVersionUID

---
+++ src/work_with_files/programmer1/SerializationEx1.java
@@ -0,0 +1,27 @@
+package work_with_files.programmer1;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SerializationEx1 {
+    public static void main(String[] args) {
+        List<String> employees = new ArrayList<>();
+        employees.add("Vit");
+        employees.add("Ivan");
+        employees.add("Zaur");
+
+        try (ObjectOutputStream outputStream
+                = new ObjectOutputStream( new FileOutputStream("employees.bin"))) {
+            outputStream.writeObject(employees);
+            System.out.println("Done!");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
