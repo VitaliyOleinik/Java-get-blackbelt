7875f72b373fbdc1682fe3239113c2cc93c1af49
VitaliyOleinik <oleynik624@gmail.com>
2023-07-13 09:48:48 +0600

    BufferedReader and BufferedWriter

---
+++ src/work_with_files/CopyEx.java
@@ -0,0 +1,29 @@
+package work_with_files;
+
+import java.io.*;
+
+public class CopyEx {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(
+                new FileReader("/home/kardin/development/Java-get-blackbelt/files/test1.txt"));
+             BufferedWriter writer = new BufferedWriter(
+                     new FileWriter("test2.txt")
+             );
+        ) {
+//            int character;
+//            while ((character = reader.read()) != -1) {
+//                writer.write(character);
+//            }
+            String line;
+            while ((line = reader.readLine()) != null) {
+                writer.write(line);
+                writer.write("\n");
+            }
+            System.out.println("Done!");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
