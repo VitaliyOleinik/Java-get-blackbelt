5fcc126fa5d24ca1216ca2ffb4af92b894720465
VitaliyOleinik <oleynik624@gmail.com>
2023-07-16 12:15:48 +0600

    Delete with Files.walkFileTree(path, new MyFileVisitor3())

---
+++ src/work_with_files/Delete.java
@@ -0,0 +1,28 @@
+package work_with_files;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class Delete {
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("/home/kardin/development/Java-get-blackbelt/CopyHere");
+        Files.walkFileTree(path, new MyFileVisitor3());
+    }
+}
+
+class MyFileVisitor3 extends SimpleFileVisitor<Path> {
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes basicFileAttributes) throws IOException {
+        System.out.println("Delete name " + file.getFileName());
+        Files.delete(file);
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException e) throws IOException {
+        System.out.println("Delete Directory " + dir.getFileName());
+        Files.delete(dir);
+        return FileVisitResult.CONTINUE;
+    }
+}
