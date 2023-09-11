06c4c3a803d6d632174f155fae6e6f7bcd0f8571
VitaliyOleinik <oleynik624@gmail.com>
2023-09-11 22:34:48 +0600

    HashSet

---
+++ src/collections/set_interface/HashSetEx.java
@@ -0,0 +1,19 @@
+package collections.set_interface;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetEx {
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+        set.add("Zaur");
+        set.add("Oleg");
+        set.add("Marina");
+        set.add("Igor");
+
+        set.remove("Zaur");
+        System.out.println(set.contains("Igor"));
+
+        System.out.println(set);
+    }
+}
